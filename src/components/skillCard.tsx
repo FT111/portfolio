7e084713bb81a2733f0f6496e6c3cfffa76e2fc1
FT111 <thetaylor2016@gmail.com>
2024-09-19 18:25:16 +0100

    Added selection indicators to skill cards

--- src/components/skillCard.tsx
+++ src/components/skillCard.tsx
@@ -34,7 +34,7 @@ export default function SkillCard({ skillTitle, skillColour, skillDescription, i
 					</DialogHeader>
 				</DialogContent>
 			</Dialog>
-			<ScrollCard onClick={() => setIsOpen(!isOpen)}
+			<ScrollCard threshold={0.5} onClick={() => setIsOpen(!isOpen)}
 						className={clsx(
 							"h-[10rem] cursor-pointer lg:grow-0 grow basis-1/3 md:basis-1/4 lg:basis-1/5",
 							"transition-all text-white rounded-2xl outline outline-[3px]",
@@ -42,18 +42,30 @@ export default function SkillCard({ skillTitle, skillColour, skillDescription, i
 							"hover:brightness-110",
 							"hover:outline-accent/50",
 							"shadow-md",
+							"group",
 							skillColour
 						)}>
+
+				<div className={"absolute flex w-full h-full p-4 justify-end align-bottom justify-items-end items-end"}>
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+						 stroke="currentColor"
+						 className="size-6 items-end opacity-50 bg-blend-lighten group-hover:opacity-100 transition-all duration-100">
+						<path stroke-linecap="round" className={"mix-blend-screen bg-blend-color-burn"} stroke-linejoin="round"
+							  d="m11.99 7.5 3.75-3.75m0 0 3.75 3.75m-3.75-3.75v16.499H4.49"/>
+					</svg>
+				</div>
 
 				<h3 className={"text-xl font-semibold p-4"}>{skillTitle}</h3>
 
 				{/* shows if icon exists */}
 				<div className={"flex flex-col justify-center items-center w-full"}>
 					{icon &&
-						<Image src={icon} alt={icon + ' icon'}
-							   width={70} height={70}/>}
+                        <Image src={icon} alt={icon + ' icon'}
+                               width={70} height={70}/>}
 				</div>
+
+
 			</ScrollCard>
-			</>
+		</>
 	);
 }
