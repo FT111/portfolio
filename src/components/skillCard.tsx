f7b386c28a922563c204f2e46e6409080db7a2c0
FT111 <thetaylor2016@gmail.com>
2024-11-15 15:54:19 +0000

    Lifted state to grid component and switched it to initialise on page mount

--- src/components/skillCard.tsx
+++ src/components/skillCard.tsx
@@ -19,165 +19,165 @@ import { Observer } from "gsap/Observer";
 
 gsap.registerPlugin(useGSAP, Flip, ScrollTrigger, Observer);
 interface SkillCardProps {
-  skillTitle: string;
-  skillColour: string;
-  skillDescription: string;
-  icon: string;
-  isExpanded: boolean;
-  onExpand: () => void;
-  onCollapse: () => void;
+	skillTitle: string;
+	skillColour: string;
+	skillDescription: string;
+	icon: string;
+	isExpanded: boolean;
+	expandedTitle: string;
+	clickHandler: () => void;
 }
 
 export default function SkillCard({
-  skillTitle,
-  skillColour,
-  skillDescription,
-  icon,
-  isExpanded,
-  onExpand,
-  onCollapse,
+	skillTitle,
+	skillColour,
+	skillDescription,
+	icon,
+	isExpanded,
+	clickHandler,
+	expandedTitle,
 }: SkillCardProps) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [_, colour, intensity] = skillColour.split("-");
-  const expandedID = `expanded-${skillTitle.split(" ")[0]}`;
-  const expandedHeaderID = `expandedHeader-${skillTitle.split(" ")[0]}`;
-  const baseID = `base-${skillTitle.split(" ")[0]}`;
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [_, colour, intensity] = skillColour.split("-");
+	const expandedID = `expanded-${skillTitle.split(" ")[0]}`;
+	const expandedHeaderID = `expandedHeader-${skillTitle.split(" ")[0]}`;
+	const baseID = `base-${skillTitle.split(" ")[0]}`;
 
-  const baseREF = React.useRef(null);
-  const expandedREF = React.useRef(null);
-  const expandedHeaderRef = React.useRef(null);
+	const baseREF = React.useRef(null);
+	const expandedREF = React.useRef(null);
+	const expandedHeaderRef = React.useRef(null);
 
-  const flip = () => {
-    const state = Flip.getState(`#${baseID}, #${expandedID}`);
+	useEffect(() => {
+		const stateBase = Flip.getState(`#${baseID}`);
+		const stateExpanded = Flip.getState(`#${expandedID}`);
 
-    baseREF.current.classList.toggle("hidden");
-    expandedREF.current.classList.toggle("hidden");
+		baseREF.current.classList.toggle("hidden");
+		expandedREF.current.classList.toggle("hidden");
 
-    Flip.from(state, {
-      duration: 0.16,
-      ease: "power1.inOut",
-      zIndex: 40,
-      fade: true,
-      absolute: true,
-    });
-  };
+		if (isExpanded) {
+			console.log(`Expanding ${skillTitle}`);
+			Flip.from(stateBase, {
+				target: stateExpanded,
+				duration: 0.16,
+				ease: "power1.inOut",
+				zIndex: 40,
+				fade: true, // Apply fade animation
+				absolute: true, // Animate as if it's positioned absolutely
+			});
+		} else if (!isExpanded && expandedTitle !== null) {
+			console.log(`Collapsing ${skillTitle}`);
+			Flip.from(stateExpanded, {
+				target: stateBase,
+				duration: 0.16,
+				ease: "power1.inOut",
+				zIndex: 40,
+				fade: true,
+				absolute: true,
+			});
+		}
+	}, [isExpanded]);
 
-  useEffect(() => {
-    // @ts-expect-error TS is wrong
-    if (!document.readyState === "complete") return;
-
-    flip();
-  }, [isExpanded, flip]);
-
-  const toggleExpand = () => {
-    if (isExpanded) {
-      onCollapse();
-    } else {
-      onExpand();
-    }
-  };
-
-  return (
-    <ScrollCard
-      threshold={0.5}
-      className={`lg:grow-0 grow basis-1/3 md:basis-1/4 lg:basis-1/5 z-10 ${isExpanded ? "z-40" : ""}`}
-    >
-      <div className={"flex flex-col justify-center"}>
-        <div
-          ref={expandedREF}
-          onClick={toggleExpand}
-          id={expandedID}
-          data-flip-id={expandedID}
-          className={`expandedCard absolute hidden w-[140%] h-[130%] -translate-x-12 translate-y-20`}
-        >
-          <div
-            className={clsx(`bg-slate-50 select-none cursor-pointer group w-full h-full rounded-2xl shadow-2xl shadow-slate-900/50 transition-all
+	return (
+		<ScrollCard
+			threshold={0.5}
+			className={`lg:grow-0 grow basis-1/3 md:basis-1/4 lg:basis-1/5 z-10 min-h-48 ${isExpanded ? "z-40" : ""}`}
+		>
+			<div className={"flex flex-col justify-center"}>
+				<div
+					ref={expandedREF}
+					onClick={clickHandler}
+					id={expandedID}
+					data-flip-id={expandedID}
+					className={`expandedCard absolute hidden w-[140%] h-[130%] -translate-x-12 translate-y-20`}
+				>
+					<div
+						className={clsx(`bg-slate-50 select-none cursor-pointer group w-full h-full rounded-2xl shadow-2xl shadow-slate-900/50 transition-all
        outline outline-[3px] hover:outline-[5px] outline-slate-300 hover:outline-accent/50`)}
-          >
-            {/* Cross SVG overlay */}
-            <div
-              className={
-                "absolute flex w-full h-full p-5 justify-end align-bottom justify-items-end items-end"
-              }
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="size-6 items-end opacity-50 bg-blend-lighten group-hover:opacity-100 transition-all duration-100"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M6 18 18 6M6 6l12 12"
-                />
-              </svg>
-            </div>
+					>
+						{/* Cross SVG overlay */}
+						<div
+							className={
+								"absolute flex w-full h-full p-5 justify-end align-bottom justify-items-end items-end"
+							}
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								className="size-6 items-end opacity-50 bg-blend-lighten group-hover:opacity-100 transition-all duration-100"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M6 18 18 6M6 6l12 12"
+								/>
+							</svg>
+						</div>
 
-            <h3
-              ref={expandedHeaderRef}
-              id={expandedHeaderID}
-              className={`text-xl select-none transition-all font-semibold p-4 w-full text-slate-50
+						<h3
+							ref={expandedHeaderRef}
+							id={expandedHeaderID}
+							className={`text-xl select-none transition-all font-semibold p-4 w-full text-slate-50
         bg-${colour}-${intensity} p-2 rounded-t-2xl`}
-            >
-              {skillTitle}
-            </h3>
-            <p className={"p-4 select-none"}>{skillDescription}</p>
-          </div>
-        </div>
+						>
+							{skillTitle}
+						</h3>
+						<p className={"p-4 select-none"}>{skillDescription}</p>
+					</div>
+				</div>
 
-        <div
-          ref={baseREF}
-          onClick={toggleExpand}
-          id={baseID}
-          data-flip-id={expandedID}
-          className={clsx(
-            "h-[10rem] cursor-pointer",
-            "transition-all text-white rounded-2xl outline outline-[3px]",
-            "hover:outline-[5px] outline-slate-300",
-            "hover:brightness-110",
-            "hover:outline-accent/50",
-            "saturate-[80%]",
-            "shadow-md",
-            "group",
-            "z-10",
-            skillColour,
-          )}
-        >
-          <div
-            className={
-              "absolute flex w-full h-full p-4 justify-end align-bottom justify-items-end items-end"
-            }
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="size-6 items-end opacity-50 bg-blend-lighten group-hover:opacity-100 transition-all duration-100"
-            >
-              <path
-                stroke-linecap="round"
-                className={"mix-blend-screen bg-blend-color-burn"}
-                stroke-linejoin="round"
-                d="m11.99 7.5 3.75-3.75m0 0 3.75 3.75m-3.75-3.75v16.499H4.49"
-              />
-            </svg>
-          </div>
-          <h3 className={"text-xl font-semibold p-4"}>{skillTitle}</h3>
-          <div className={"flex flex-col justify-center items-center w-full"}>
-            {icon && (
-              <Image src={icon} alt={icon + " icon"} width={70} height={70} />
-            )}
-          </div>
-          <p className={"hidden blur-lg"} data-flip-id={expandedID}>
-            {skillDescription}
-          </p>
-        </div>
-      </div>
-    </ScrollCard>
-  );
+				<div
+					ref={baseREF}
+					onClick={clickHandler}
+					id={baseID}
+					data-flip-id={expandedID}
+					className={clsx(
+						"h-[10rem] cursor-pointer",
+						"transition-all text-white rounded-2xl outline outline-[3px]",
+						"hover:outline-[5px] outline-slate-300",
+						"hover:brightness-110",
+						"hover:outline-accent/50",
+						"saturate-[80%]",
+						"shadow-md",
+						"group",
+						"z-10",
+						skillColour,
+					)}
+				>
+					<div
+						className={
+							"absolute flex w-full h-full p-4 justify-end align-bottom justify-items-end items-end"
+						}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							className="size-6 items-end opacity-50 bg-blend-lighten group-hover:opacity-100 transition-all duration-100"
+						>
+							<path
+								stroke-linecap="round"
+								className={"mix-blend-screen bg-blend-color-burn"}
+								stroke-linejoin="round"
+								d="m11.99 7.5 3.75-3.75m0 0 3.75 3.75m-3.75-3.75v16.499H4.49"
+							/>
+						</svg>
+					</div>
+					<h3 className={"text-xl font-semibold p-4"}>{skillTitle}</h3>
+					<div className={"flex flex-col justify-center items-center w-full"}>
+						{icon && (
+							<Image src={icon} alt={icon + " icon"} width={70} height={70} />
+						)}
+					</div>
+					<p className={"hidden blur-lg"} data-flip-id={expandedID}>
+						{skillDescription}
+					</p>
+				</div>
+			</div>
+		</ScrollCard>
+	);
 }
--- src/components/skillGrid.tsx
+++ src/components/skillGrid.tsx
@@ -1,37 +1,83 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SkillCard from "@/components/skillCard";
 
 export default function SkillGrid({ skills }) {
-  const [expandedCard, setExpandedCard] = useState<string | null>(null);
+	const [expandedCard, setExpandedCard] = useState<string | null>(null);
+	const [cardStates, setCardStates] = useState<{ [key: string]: boolean }>({});
 
-  const handleExpand = (title: string) => {
-    setExpandedCard((prev) => (prev === title ? null : title));
-  };
+	useEffect(() => {
+		const initialCardStates = skills.reduce(
+			(acc: { [key: string]: boolean }, skill: { title: string }) => {
+				acc[skill.title] = false;
+				return acc;
+			},
+			{},
+		);
+		setCardStates(initialCardStates);
+	}, [skills]);
 
-  return (
-    <>
-      {skills.map(
-        (
-          skill: {
-            title: string;
-            colour: string;
-            description: string;
-            icon: string;
-          },
-          index: React.Key,
-        ) => (
-          <SkillCard
-            key={index}
-            skillTitle={skill.title}
-            skillColour={skill.colour}
-            skillDescription={skill.description}
-            icon={skill.icon}
-            isExpanded={expandedCard === skill.title}
-            onExpand={() => handleExpand(skill.title)}
-            onCollapse={() => setExpandedCard(null)}
-          />
-        ),
-      )}
-    </>
-  );
+	// Handle the click event for the card
+	const handleClick = (title: string) => {
+		console.log(`Clicked ${title}!!!!`);
+
+		// Toggle the card state
+		setCardStates((prevCardStates) => ({
+			...prevCardStates,
+			[title]: !prevCardStates[title],
+		}));
+
+		// If the card is already expanded, collapse it
+		if (expandedCard !== null && expandedCard !== title) {
+			setCardStates((prevCardStates) => ({
+				...prevCardStates,
+				[expandedCard]: false,
+			}));
+			console.log(`Collapsing card: ${expandedCard}`);
+		}
+
+		// Mark as expanded
+		setExpandedCard((prevExpandedCard) => {
+			if (prevExpandedCard === null || prevExpandedCard !== title) {
+				console.log(`Expanding card: ${title}`);
+				return title;
+			} else {
+				console.log(`Set expanded card to null`);
+				return null;
+			}
+		});
+	};
+
+	// Log the state changes
+	useEffect(() => {
+		console.info("Current card states: ", cardStates);
+		console.log("Expanded card: ", expandedCard);
+		console.log("------------------------------------------------------");
+	}, [cardStates, expandedCard]);
+
+	return (
+		<>
+			{skills.map(
+				(
+					skill: {
+						title: string;
+						colour: string;
+						description: string;
+						icon: string;
+					},
+					index: React.Key,
+				) => (
+					<SkillCard
+						key={index}
+						skillTitle={skill.title}
+						skillColour={skill.colour}
+						skillDescription={skill.description}
+						icon={skill.icon}
+						isExpanded={cardStates[skill.title]}
+						clickHandler={() => handleClick(skill.title)}
+						expandedTitle={expandedCard}
+					/>
+				),
+			)}
+		</>
+	);
 }
