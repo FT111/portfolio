dfba33bfdf031681b31dad4258491c2ecebc7699
FT111 <thetaylor2016@gmail.com>
2024-11-17 23:01:26 +0000

    Added github URLs to projects

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import Image from "next/image";
 import ScrollCard from "@/components/scrollCard";
-import { ProjectStack } from "@/lib/types";
+import { Project } from "@/lib/types";
 
 // Object of stack title and an icon SVG
 
 interface Props {
-	children?: React.ReactNode;
-	className?: string;
-	stack?: ProjectStack;
-	imageHREF?: string;
-	blurredHREF?: string;
+	project: Project;
 	alternate?: boolean;
+	children: React.ReactNode;
 }
 
 export default function ProjectCard({ children, ...props }: Props) {
@@ -26,7 +23,7 @@ export default function ProjectCard({ children, ...props }: Props) {
 			<div
 				className={`${props.alternate ? "md:order-1" : ""} flex flex-col md:flex-row rounded-2xl basis-1/2`}
 			>
-				{props.imageHREF && (
+				{props.project.imageHREF && (
 					<div
 						className={
 							`grid grid-cols-1 basis-8/12 grid-rows-1 grow items-center ${props.alternate ? `md:order-2 sm:rounded-tl-none sm:rounded-r-2xl` : `sm:rounded-tr-none sm:rounded-l-2xl`} ` +
@@ -37,7 +34,7 @@ export default function ProjectCard({ children, ...props }: Props) {
 							style={{
 								gridRow: 1,
 								gridColumn: 1,
-								backgroundImage: `url(${props.blurredHREF})`,
+								backgroundImage: `url(${props.project.blurredHREF})`,
 								backgroundSize: "cover",
 							}}
 							className={
@@ -52,7 +49,7 @@ export default function ProjectCard({ children, ...props }: Props) {
 						>
 							<Image
 								className={"rounded-xl z-20 h-auto w-full shadow-md"}
-								src={props.imageHREF}
+								src={props.project.imageHREF}
 								alt={"Image of a project"}
 								width={1920}
 								height={1080}
@@ -73,8 +70,8 @@ export default function ProjectCard({ children, ...props }: Props) {
 					>
 						Stack
 					</p>
-					{props.stack &&
-						props.stack.map((stack, i) => (
+					{props.project.stack &&
+						props.project.stack.map((stack, i) => (
 							<div
 								key={i}
 								className={`flex w-20 h-auto gap-3 md:group-hover:w-56 items-center p-4 transition-all ${props.alternate ? "flex-row" : "flex-row-reverse"}  `}
@@ -100,7 +97,30 @@ export default function ProjectCard({ children, ...props }: Props) {
 				</div>
 			</div>
 
-			<div className={"basis-1/2 p-10 text-slate-900"}>{children}</div>
+			<div className={"basis-1/2 p-5 flex flex-col justify-between"}>
+				<div className={`p-5 text-slate-900 flex flex-col h-full `}>
+					{children}
+				</div>
+				{/*	Github URL */}
+				<a
+					href={props.project.githubURL}
+					className={"justify-self-end self-end"}
+				>
+					<svg
+						className={"scale-75 opacity-90"}
+						width="98"
+						height="96"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							fill-rule="evenodd"
+							clip-rule="evenodd"
+							d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+							fill="#24292f"
+						/>
+					</svg>
+				</a>
+			</div>
 		</ScrollCard>
 	);
 }
